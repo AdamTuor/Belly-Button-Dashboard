25cde63928d747d3e5023c7771ddcba78d871840
Adam Tuor <tuora99@gmail.com>
2023-03-29 23:25:31 -0400

    added ugly gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -4,15 +4,15 @@ const dataPromise = d3.json(url);
 // If a new sample is selected just call the barChart method with the new value
 function optionChanged(newSample)
 {
-    barChart(newSample);
+    charts(newSample);
 }
 
-function barChart(sample)
+function charts(sample)
 {
     panel = d3.select('#sample-metadata')
     d3.json(url).then(function(data)
     {   
-        // Read in data and assign the samples array to samples
+        // Read in data and assign to variable
         let samples = data.samples;
         let metaData = data.metadata
         // Filter to find the sample that was selected
@@ -22,16 +22,18 @@ function barChart(sample)
         let selectedSample = findSample[0];
         let selectedMeta = findMeta[0];
         // Get the top 10 otu_id, labels, and sample values
-        let otu_ids = selectedSample.otu_ids.slice(0,10).reverse();
-        let otu_labels = selectedSample.otu_labels.slice(0,10).reverse();
-        let sample_values = selectedSample.sample_values.slice(0,10).reverse();
+        let top10_otu_ids = selectedSample.otu_ids.slice(0,10).reverse();
+        let top10_otu_labels = selectedSample.otu_labels.slice(0,10).reverse();
+        let top10_sample_values = selectedSample.sample_values.slice(0,10).reverse();
+        let otu_ids = selectedSample.otu_ids;
+        let otu_labels = selectedSample.otu_labels;
+        let sample_values = selectedSample.sample_values;
 
-        /*selectedMeta.forEach(function(entry)
-        {
-            panel.append("row").text(entry).property("values",entry)
-        });*/
+        
+          
         // clear the panel or it just keeps adding info for each selection
         panel.html("")
+        // convert the selectedMeta object into key-value pairs and add the pairs to the panel
         Object.entries(selectedMeta).forEach(([key, value])=>
         {
             panel.append("p").text(`${key}:${value}`);
@@ -40,9 +42,9 @@ function barChart(sample)
         // Setup variables for the chart
         let barTrace=
         {
-            x:sample_values,
-            y:otu_ids.map(id => `OTU ${id}`),
-            text: otu_labels,
+            x:top10_sample_values,
+            y:top10_otu_ids.map(id => `OTU ${id}`),
+            text: top10_otu_labels,
             type: "bar",
             orientation: "h"
         };
@@ -63,6 +65,33 @@ function barChart(sample)
 
         let barData = [barTrace];
         let bubbleData = [bubbleTrace];
+        let gaugeData = [
+            {
+              domain: { x: [0, 1], y: [0, 1] },
+              value: selectedMeta.wfreq,
+              title: { text: "Belly Button Washing Frequency" },
+              type: "indicator",
+              mode: "gauge+number",
+              gauge: 
+              { 
+                steps: [
+                    { range: [0, 1], color: "DarkRed" },
+                    { range: [1, 2], color: "Red" },
+                    { range: [2, 3], color: "OrangeRed" },
+                    { range: [3, 4], color: "DarkOrange" },
+                    { range: [4, 5], color: "Gold" },
+                    { range: [5, 6], color: "Yellow" },
+                    { range: [6, 7], color: "Green" },
+                    { range: [7, 8], color: "LimeGreen" },
+                    { range: [8, 9], color: "LawnGreen" },
+                  ],
+                axis: 
+                { 
+                    range: [null, 9] 
+                } 
+              }
+            }
+          ];
         let barLayout =
         {
             title: `Top 10 OTUs found in subject ${sample}`
@@ -72,9 +101,11 @@ function barChart(sample)
         {
             title: `Bubble chart for ${sample}`
         };
+        let gaugeLayout ={ width: 600, height: 400 };
         // Plot the chart
         Plotly.newPlot("bar",barData,barLayout);
         Plotly.newPlot("bubble",bubbleData,bubbleLayout);
+        Plotly.newPlot("gauge",gaugeData,gaugeLayout);
     });
 }
 
@@ -106,7 +137,7 @@ function setup()
         });
         // use the first value in names to populate the charts
         let startSample = names[0];
-        barChart(startSample);
+        charts(startSample);
     });
 }
 
